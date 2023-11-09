71948052d39203511cabed2137c5eda4309fb87a
Bellorico323 <mabo.orico@gmail.com>
2023-11-09 15:58:45 -0300

    selecionando os elementos

---
+++ script.js
@@ -0,0 +1,12 @@
+//Images
+const containerHeader = document.getElementById("header");
+const secondTextImage = document.querySelector('[data-image="image"]');
+const bodyDocument = document.body;
+const fistTextParagraphs = document.querySelector('[data-text="first-text"]')
+const secondText = document.querySelector('[data-text="second-text"]')
+const divider = document.querySelector('.divider')
+const containerFooter = document.getElementById("footer");
+
+
+//container.style.backgroundImage = 'url("./assets/black-and-blue-background.jpg")'
+// secondTextImage.setAttribute('src', './assets/black-and-blue -background.jpg');
