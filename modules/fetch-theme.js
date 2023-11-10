092fe2e8be42e15f57bd6ab949c85bab2960cca2
Bellorico323 <mabo.orico@gmail.com>
2023-11-10 18:38:26 -0300

    rodrigo pretel

--- modules/fetch-theme.js
+++ modules/fetch-theme.js
@@ -10,7 +10,8 @@ const secondText = document.querySelector('[data-text="second-text"]')
 const divider = document.querySelector('.divider')
 const containerFooter = document.getElementById("footer");
 
-const themes = document.querySelectorAll('.theme-list')
+const titleDetail = document.querySelector('.title-container .detail')
+
 //container.style.backgroundImage = 'url("./assets/black-and-blue-background.jpg")'
 // secondTextImage.setAttribute('src', './assets/black-and-blue -background.jpg');
 async function serchTheme(){
@@ -26,7 +27,15 @@ async function serchTheme(){
         document.querySelector('.theme-list').appendChild(li);
         
         function changeTheme(){
-          h1Header.style.backgroundImage = `url(${theme[index].image-bg})`;
+          containerHeader.style.backgroundImage = `url(${theme.imageBg})`;
+          h1Header.style.color = theme.titleBg;
+          secondTextImage.setAttribute('src', theme.imageBg);
+          bodyDocument.style.background = theme.background;
+          fistTextParagraphs.style.color = theme.text;
+          secondText.style.color = theme.text; 
+          divider.style.background = theme.color;
+          titleDetail.style.background = theme.color;
+          containerFooter.style.backgroundImage = `url(${theme.imageBg})`
         }
         
         li.addEventListener('click', changeTheme)
